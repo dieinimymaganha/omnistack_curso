9e1673d74c1fba21f2a780a8242c30c76f19a0c3
Dieinimy Maganha <dieinimy@gmail.com>
2019-10-15 22:32:59 -0300

    Enviando o projeto

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes')
+
+const server = express();
+
+mongoose.connect('mongodb+srv://root:1234@cluster0-pqen6.mongodb.net/oministack8?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
+
+
+
+server.use(express.json());
+
+server.use(routes);
+
+server.listen(3333);
