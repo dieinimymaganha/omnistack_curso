0dd841afb7072ce7f8a0a3cc63312fbd4450f4c2
Dieinimy Maganha <dieinimy@gmail.com>
2019-10-16 13:27:40 -0300

    Criando rota para dislikes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,10 +4,12 @@ const routes = express.Router();
 
 const DevController = require('./controllers/DevController');
 const LikeController = require('./controllers/LikeController');
+const DislikeController = require('./controllers/DislikeController');
 
 
 
 routes.post('/devs', DevController.store);
 routes.post('/devs/:devId/likes', LikeController.store);
+routes.post('/devs/:devId/dislikes', DislikeController.store);
 
 module.exports = routes;
