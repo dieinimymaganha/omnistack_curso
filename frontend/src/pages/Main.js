857b6d824a30835968688892a6023405750a767b
Dieinimy Maganha <dieinimy@gmail.com>
2019-10-20 09:30:06 -0300

    Criando a pagina das lista dos Devs, mostrando manualmente os dados

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,5 +1,106 @@
 import React from 'react';
+import './Main.css';
 
-export default function Routes(){
- return <h1>Hello World</h1>   
+import logo from '../assets/logo.svg'
+import dislike from '../assets/dislike.svg'
+import like from '../assets/like.svg'
+
+export default function Main({ match }){
+ return (
+     <div className="main-container">
+         <img src={logo} alt="Tindev"/>
+         <ul>
+             <li>
+                 <img src="https://avatars0.githubusercontent.com/u/38332691?v=4" alt=""/>
+                 <footer>
+                     <strong>Dieinimy Maganha</strong>
+                     <p>Programador bichão mesmo</p>
+                 </footer>
+                 <div className="buttons">
+                     <button type="button">
+                         <img src={dislike} alt="Dislike"/>
+                     </button>
+                     <button type="button">
+                         <img src={like} alt="like"/>
+                     </button>
+                 </div>
+             </li>
+             <li>
+                 <img src="https://avatars0.githubusercontent.com/u/38332691?v=4" alt=""/>
+                 <footer>
+                     <strong>Dieinimy Maganha</strong>
+                     <p>Programador bichão mesmo</p>
+                 </footer>
+                 <div className="buttons">
+                     <button type="button">
+                         <img src={dislike} alt="Dislike"/>
+                     </button>
+                     <button type="button">
+                         <img src={like} alt="like"/>
+                     </button>
+                 </div>
+             </li>
+             <li>
+                 <img src="https://avatars0.githubusercontent.com/u/38332691?v=4" alt=""/>
+                 <footer>
+                     <strong>Dieinimy Maganha</strong>
+                     <p>Programador bichão mesmo</p>
+                 </footer>
+                 <div className="buttons">
+                     <button type="button">
+                         <img src={dislike} alt="Dislike"/>
+                     </button>
+                     <button type="button">
+                         <img src={like} alt="like"/>
+                     </button>
+                 </div>
+             </li>
+             <li>
+                 <img src="https://avatars0.githubusercontent.com/u/38332691?v=4" alt=""/>
+                 <footer>
+                     <strong>Dieinimy Maganha</strong>
+                     <p>Programador bichão mesmo</p>
+                 </footer>
+                 <div className="buttons">
+                     <button type="button">
+                         <img src={dislike} alt="Dislike"/>
+                     </button>
+                     <button type="button">
+                         <img src={like} alt="like"/>
+                     </button>
+                 </div>
+             </li>
+             <li>
+                 <img src="https://avatars0.githubusercontent.com/u/38332691?v=4" alt=""/>
+                 <footer>
+                     <strong>Dieinimy Maganha</strong>
+                     <p>Programador bichão mesmo</p>
+                 </footer>
+                 <div className="buttons">
+                     <button type="button">
+                         <img src={dislike} alt="Dislike"/>
+                     </button>
+                     <button type="button">
+                         <img src={like} alt="like"/>
+                     </button>
+                 </div>
+             </li>
+             <li>
+                 <img src="https://avatars0.githubusercontent.com/u/38332691?v=4" alt=""/>
+                 <footer>
+                     <strong>Dieinimy Maganha</strong>
+                     <p>Programador bichão mesmo</p>
+                 </footer>
+                 <div className="buttons">
+                     <button type="button">
+                         <img src={dislike} alt="Dislike"/>
+                     </button>
+                     <button type="button">
+                         <img src={like} alt="like"/>
+                     </button>
+                 </div>
+             </li>
+         </ul>
+     </div>
+ )
 }
