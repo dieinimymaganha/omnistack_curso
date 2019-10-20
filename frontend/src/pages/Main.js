01e6aaa821c4d375c2f33156baac1ac3709cb570
Dieinimy Maganha <dieinimy@gmail.com>
2019-10-20 09:54:03 -0300

    Pegando as informações dos usuários diretamente da API

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,106 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Main.css';
+
+import api from '../services/api';
 
 import logo from '../assets/logo.svg'
 import dislike from '../assets/dislike.svg'
 import like from '../assets/like.svg'
 
-export default function Main({ match }){
- return (
-     <div className="main-container">
-         <img src={logo} alt="Tindev"/>
-         <ul>
-             <li>
-                 <img src="https://avatars0.githubusercontent.com/u/38332691?v=4" alt=""/>
-                 <footer>
-                     <strong>Dieinimy Maganha</strong>
-                     <p>Programador bichão mesmo</p>
-                 </footer>
-                 <div className="buttons">
-                     <button type="button">
-                         <img src={dislike} alt="Dislike"/>
-                     </button>
-                     <button type="button">
-                         <img src={like} alt="like"/>
-                     </button>
-                 </div>
-             </li>
-             <li>
-                 <img src="https://avatars0.githubusercontent.com/u/38332691?v=4" alt=""/>
-                 <footer>
-                     <strong>Dieinimy Maganha</strong>
-                     <p>Programador bichão mesmo</p>
-                 </footer>
-                 <div className="buttons">
-                     <button type="button">
-                         <img src={dislike} alt="Dislike"/>
-                     </button>
-                     <button type="button">
-                         <img src={like} alt="like"/>
-                     </button>
-                 </div>
-             </li>
-             <li>
-                 <img src="https://avatars0.githubusercontent.com/u/38332691?v=4" alt=""/>
-                 <footer>
-                     <strong>Dieinimy Maganha</strong>
-                     <p>Programador bichão mesmo</p>
-                 </footer>
-                 <div className="buttons">
-                     <button type="button">
-                         <img src={dislike} alt="Dislike"/>
-                     </button>
-                     <button type="button">
-                         <img src={like} alt="like"/>
-                     </button>
-                 </div>
-             </li>
-             <li>
-                 <img src="https://avatars0.githubusercontent.com/u/38332691?v=4" alt=""/>
-                 <footer>
-                     <strong>Dieinimy Maganha</strong>
-                     <p>Programador bichão mesmo</p>
-                 </footer>
-                 <div className="buttons">
-                     <button type="button">
-                         <img src={dislike} alt="Dislike"/>
-                     </button>
-                     <button type="button">
-                         <img src={like} alt="like"/>
-                     </button>
-                 </div>
-             </li>
-             <li>
-                 <img src="https://avatars0.githubusercontent.com/u/38332691?v=4" alt=""/>
-                 <footer>
-                     <strong>Dieinimy Maganha</strong>
-                     <p>Programador bichão mesmo</p>
-                 </footer>
-                 <div className="buttons">
-                     <button type="button">
-                         <img src={dislike} alt="Dislike"/>
-                     </button>
-                     <button type="button">
-                         <img src={like} alt="like"/>
-                     </button>
-                 </div>
-             </li>
-             <li>
-                 <img src="https://avatars0.githubusercontent.com/u/38332691?v=4" alt=""/>
-                 <footer>
-                     <strong>Dieinimy Maganha</strong>
-                     <p>Programador bichão mesmo</p>
-                 </footer>
-                 <div className="buttons">
-                     <button type="button">
-                         <img src={dislike} alt="Dislike"/>
-                     </button>
-                     <button type="button">
-                         <img src={like} alt="like"/>
-                     </button>
-                 </div>
-             </li>
-         </ul>
-     </div>
- )
+export default function Main({ match }) {
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        async function loadUsers() {
+            const response = await api.get('/devs', {
+                headers: {
+                    user: match.params.id,
+                }
+            })
+
+            setUsers(response.data);
+
+        }
+        loadUsers();
+    }, [match.params.id])
+    return (
+        <div className="main-container">
+            <img src={logo} alt="Tindev" />
+            <ul>
+                {users.map(user => (
+                    <li key={user._id}>
+                        <img src={user.avatar} alt={user.name} />
+                        <footer>
+                            <strong>{user.name}</strong>
+                            <p>{user.bio}</p>
+                        </footer>
+                        <div className="buttons">
+                            <button type="button">
+                                <img src={dislike} alt="Dislike" />
+                            </button>
+                            <button type="button">
+                                <img src={like} alt="like" />
+                            </button>
+                        </div>
+                    </li>
+                ))}
+
+            </ul>
+        </div>
+    )
 }
